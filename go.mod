657d0cd93e53797cb5c7ed0e48952d3870e0116e
BragdonD <thomasduclos1012@gmail.com>
2024-09-12 08:11:00 +0200

    feat: start defining internal router structure

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/BragdonD/stock-products
 
 go 1.22.3
+
+require github.com/bragdond/simple-rest v0.1.0 // indirect
--- http.go
+++ http.go
@@ -1,12 +1,54 @@
 package stockproducts
 
-import simplerest "github.com/bragdond/simple-rest"
+import (
+	"fmt"
+	"net/http"
 
-type Router struct {
+	simplerest "github.com/bragdond/simple-rest"
+)
+
+// router is the structure that holds all the datas relative to
+// HTTP requests.
+type router struct {
 	server *simplerest.Server
 }
 
-func NewRouter() (Router, error) {
+// NewRouter initializes a new router for the given server and registers
+// the necessary routes for handling product-related HTTP requests.
+func NewRouter(server *simplerest.Server) (*router, error) {
+	router := &router{
+		server: server,
+	}
 
-	return Router{}, nil
+	if err := router.server.HandleFunc("/products",
+		nil,
+		handleProducts, http.MethodGet, http.MethodPost); err != nil {
+		return nil, fmt.Errorf("unable to register route %s on the server: %v", "/products", err)
+	}
+
+	if err := router.server.HandleFunc("/products/{uuid}",
+		nil,
+		handleProduct, http.MethodGet, http.MethodPut, http.MethodPatch, http.MethodDelete); err != nil {
+		return nil, fmt.Errorf("unable to register route %s on the server: %v", "/products/{uuid}", err)
+	}
+
+	if err := router.server.HandleFunc("/products/search",
+		nil,
+		handleSearch, http.MethodGet); err != nil {
+		return nil, fmt.Errorf("unable to register route %s on the server: %v", "/products/search", err)
+	}
+
+	return router, nil
+}
+
+func handleProducts(w http.ResponseWriter, req *http.Request, params simplerest.Parameters) error {
+	return nil
+}
+
+func handleProduct(w http.ResponseWriter, req *http.Request, params simplerest.Parameters) error {
+	return nil
+}
+
+func handleSearch(w http.ResponseWriter, req *http.Request, params simplerest.Parameters) error {
+	return nil
 }
