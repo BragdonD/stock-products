a1b706388e9fe0d6ac88022a49217735242b8d1b
BragdonD <thomasduclos1012@gmail.com>
2024-09-13 22:00:37 +0200

    chore: start to define dto types

--- usecase.go
+++ usecase.go
@@ -1,1 +1,28 @@
 package stockproducts
+
+// Product represents any item in the company inventory
+type Product struct {
+	Uuid        string
+	Name        string
+	Reference   string
+	Line        string
+	Supplier    string
+	Description string
+	ImageUrl    string
+	Variants    []ProductVariant
+}
+
+// ProductVariant represents subcategory of item such as for
+// example an item that could be either in red or blue color.
+type ProductVariant struct {
+	// Size correspond to item size such as S, M, L, XL, etc...
+	Size  *string
+	Color *string
+	// Length corresponds to the original length of the item
+	// (e.g 50m joint spool)
+	Length   *int
+	Quantity int
+}
+
+type ProductSize struct {
+}
